3bc614bb2998fc8ad9ad48fa5ffbd14668b41baa
Robbie <robbie.huffman@nundrum.net>
2025-04-11 18:09:56 -0400

    added shell subcommand

--- src/way/main.clj
+++ src/way/main.clj
@@ -1,7 +1,9 @@
 (ns way.main
  (:require [way.notecheck :as notecheck]
            [way.runner :as runner]
+           [way.shell :as shell]
 ;           [way.edit :as edit]
+;           [way.search :as search]
           ; [babashka.cli :as cli]
            )
 )
@@ -10,6 +12,8 @@
   "notecheck" (notecheck/check (rest args))
   "show"      (notecheck/show (rest args))
   "run"       (runner/run (rest args))
+  "shell"     (shell/shell (rest args))
+;  "search"    (search/search (rest args))
 ;  "edit"      (edit/edit (rest args))
   ;"keep" - keep shell history?
   (println "no match")
---
+++ src/way/shell.clj
@@ -0,0 +1,23 @@
+(ns way.shell
+  (:require [clojure.java.io :as io]
+            [babashka.fs :as fs]
+            [way.terminal :as term]
+            [clojure.edn :as edn]))
+
+(defn help [args]
+  (case (first args)
+    "bash" "run: eval $( way shell bash )"
+    " the 'shell' command outputs commands to integrate the wayfinder into your shell\n try 'way shell bash' or 'way help bash'\n supported shells: bash"
+    ))
+
+(defn shell [args]
+  (println 
+    (case (first args)
+      "help" (help (rest args))
+      "bash" "function cd_wayfinder() { cd \"$@\" && [ -f .wayf ] && way notecheck; }; alias cd=cd_wayfinder"
+      "other shells unsupported as of yet; try 'way shell help'"
+    )
+  )
+)
+
+(help ["bash"])
