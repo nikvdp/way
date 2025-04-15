8646f1fa8fcefafd3a76cc33ffd3b0b7a742d890
Robbie <robbie.huffman@nundrum.net>
2025-04-14 21:46:35 -0400

    adding server capability

---
+++ src/way/server.clj
@@ -0,0 +1,29 @@
+(ns way.server
+  (:require [clojure.java.io :as io])
+  (:import [java.net ServerSocket Socket])
+  )
+
+(defn serve [socket]
+  (with-open [client (.accept socket)]
+    (let [out (io/writer (.getOutputStream client))
+          in  (io/reader (.getInputStream client))
+         ]
+      (println "Connection accepted from: " (.getRemoteSocketAddress client))
+      (println "Message: "
+               (.read (java.io.PushbackReader. in))
+               (.write out "You have found your way")
+               (.flush out)
+      ))))
+
+(defn systemd-serve []
+  (let [listen-fd (Integer/parseInt (System/getenv "LISTEN_FDS"))
+        server-socket (java.net.ServerSocket. nil nil listen-fd)]
+    (println "Systemd activated server listening on: " listen-fd)))
+
+(defn cli-serve [args]
+  (with-open [ss (new ServerSocket 3250 )]
+    (println "CLI activated server listening on: 3250")
+    (serve ss)))
+
+(defn sync []
+  )
