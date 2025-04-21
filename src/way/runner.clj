98ead3ad3741848eaffc2cde62542d801ebdcd58
Robbie <robbie.huffman@nundrum.net>
2025-04-20 22:31:05 -0400

    runner can exec commands now

--- src/way/runner.clj
+++ src/way/runner.clj
@@ -1,25 +1,40 @@
 (ns way.runner
   (:require [clojure.java.io :as io]
             [babashka.fs :as fs]
+            [babashka.process :as ps]
+            [way.conf :as conf]
             [clojure.edn :as edn]))
 
+(defn exec [cmdstr]
+  (let [
+        process-builder (java.lang.ProcessBuilder. (list "sh" "-c" cmdstr))
+        inherit         (java.lang.ProcessBuilder$Redirect/INHERIT)
+       ]
+    (.redirectOutput process-builder inherit)
+    (.redirectInput process-builder inherit)
+    (.redirectError process-builder inherit)
+    (.waitFor (.start process-builder))
+  ))
+
 (defn run [args]
- (when (fs/exists? ".wayf")
-    (let [dotfile (fs/file ".wayf")
-          notes (-> dotfile slurp edn/read-string)
+ (when-let [wayf (conf/find-wayf)]
+    (let [
+          notes (-> wayf slurp edn/read-string)
           commands (-> notes :commands keys)
-          command-str (->> commands (map symbol) (map str) 
-                         (interleave (repeat " ")) (apply str))
           ]
+
+      ;print out list of commands when none is given
       (when (empty? args)
-        (println "Please choose which usage command to run" command-str)
+        (doseq [command commands]
+          (println (format "\t%s: %s" (symbol command) (get-in notes [:commands command]))))
         (System/exit 1))
 
-      (doseq [arg args
-            :let [karg (keyword arg)
-                  message-lines (-> notes :usage-reminders karg)]]
-        (doseq [line message-lines]
-                (println line))
-        (println))
+      (let [command (get-in notes [:commands (keyword (first args))])]
+        (when (nil? command)
+          (println (str "Could not find command to match '" (first args) "' in .wayf"))
+          (System/exit 1))
+        (exec command)
+        )
       )) 
 )
+
