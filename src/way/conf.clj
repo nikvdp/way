438725011a957c1c9f2b1c2b397313f387e92916
Robbie <robbie.huffman@nundrum.net>
2025-04-30 21:57:57 -0400

    fixed recursion; added create function for .wayf

--- src/way/conf.clj
+++ src/way/conf.clj
@@ -3,14 +3,34 @@
             [babashka.fs :as fs]
             [clojure.edn :as edn]))
 
-(defn find-config []
+(defn find-wayf 
+  "Search recursively upward for .wayf file"
+  []
   (let [cwd (fs/cwd)]
     (loop [wd cwd]
+      (println wd)
       (cond 
-        (.exists (fs/file cwd ".wayf")) (fs/file (fs/cwd) ".wayf")
-        (nil? (fs/parent cwd))          nil
-        :else (recur (fs/parent cwd))
+        (.exists (fs/file wd ".wayf")) (fs/file wd ".wayf")
+        (nil? (fs/parent wd))          nil 
+        :else (recur (fs/parent wd))
         ))
     ))
 
+(def template-wayf 
+  {:search-keys []
+   :direntry-note "Note displayed upon entry to this directory"
+   :usage-reminders {:example ["Structured usage reminders"]}
+   :commands {:lsa "ls -a"}
+   })
 
+(defn create-wayf []
+  (spit ".wayf" template-wayf)
+  (fs/file ".wayf")
+  )
+
+(defn expert
+  "Set or toggle 'expert mode' setting"
+  [args]
+
+
+  )
