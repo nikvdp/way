f7768e1b8a874cfe91e285ef0e72eeb396e807f4
Robbie <robbie.huffman@nundrum.net>
2025-04-30 22:00:40 -0400

    editing for .wayf file works

--- src/way/edit.clj
+++ src/way/edit.clj
@@ -1,6 +1,9 @@
 (ns way.edit
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn])
+            [clojure.edn :as edn]
+            [way.conf :as conf]
+            [borkdude.rewrite-edn :as redn]
+            )
   )
 
 (defn launch-editor [filename]
@@ -14,23 +17,94 @@
     (.waitFor (.start process-builder))
   ))
 
-(defn new [args]
-  (launch-editor)
-    )
+(def comments 
+  {:direntry-note 
+   "#Enter text to be displayed upon entry to this directory"
+   :usage-reminders
+   "#Enter your usage reminder"
+   :commands
+   "#Enter sh command string"
+   :reminders
+   "#Enter your reminders below"
+   })
 
-
-(defn edit-direntry []
+(defn do-edit [wayf path-vec]
   (let [tmpfile (java.io.File/createTempFile "wayfinder-direntry" "txt")
-        tmpname (str tmpfile)]
-    (spit tmpfile "#Enter text to be displayed upon entry to this directory\n#All comment lines such as this one will be stripped")
+        tmpname (str tmpfile)
+        oldconf (-> wayf slurp edn/read-string)
+        oldtext (get-in oldconf path-vec "")
+        comment-text (get comments (first path-vec))
+        ]
+    (println oldtext)
+    ;prep file to be edited
+    (condp instance? oldtext
+      String  
+        (spit tmpfile (str comment-text "\n" oldtext))
+      clojure.lang.PersistentVector 
+        (spit tmpfile (apply str comment-text "\n" (interpose "\n" oldtext)))
+      (spit tmpfile (str comment-text "\n" oldtext))
+      )
+
+    ;edit
     (launch-editor tmpname)
-    ;TODO merge content of tmpfile to .wayf file
-    (let [wayf (-> ".wayf" slurp edn/read-string)])
 
-    ))
+    ;read in changes
+    (let [newtext (->> tmpfile io/reader line-seq 
+                       (remove #(re-matches #"#.*" %)) 
+                       )
+          newtext (condp instance? oldtext
+                    String 
+                       (->> newtext (interpose "\n" )
+                           (apply str))
+                    clojure.lang.PersistentVector 
+                       (vec newtext))
+          nodes   (redn/parse-string (slurp wayf))
+          ]
+      (spit wayf (str (redn/assoc-in nodes path-vec newtext)))
+      )
+    )
+  )
 
+;(let [estr (slurp (conf/find-wayf))
+;      nodes (redn/parse-string estr)
+;      ]
+;  (str (redn/assoc-in nodes [:commands :foo ]"do foo"))
+;  )
+
+
+;map more convenient cli names to key names used in .wayf file
+(def edit-type-map {:direntry :direntry-note
+                     :usage :usage-reminders
+                     :command :commands
+                     })
+(def edit-keys (set (vals edit-type-map)))
+(def edit-depth-map {:direntry-note 1
+                     :usage-reminders 2
+                     :commands 2
+                     })
 (defn edit [args]
-  (case (first args)
-    "direntry" (edit-direntry)
+  (let [arg-keys (mapv keyword args)
+        edit-type (get edit-type-map (first arg-keys))
+        arg-keys (assoc arg-keys 0 edit-type)
+        wayf    (or (conf/find-wayf) (conf/create-wayf))
+        ]
+    (when-not (contains? edit-keys edit-type)
+      (print "available edit subkey types: ")
+      (println (apply str (interpose " " (map name (keys edit-type-map)))))
+      (System/exit 1)
+      )
+    (when (< (count args) 
+             (get edit-depth-map (first arg-keys)))
+      (println (str (first args) " requires a subkey") )
+      (System/exit 1)
+      )
+    (do-edit wayf arg-keys)
     ))
 
+
+;(let [estr (slurp (conf/find-wayf))
+;      nodes (redn/parse-string estr)
+;      ]
+;  (str (redn/assoc-in nodes [:commands :foo ]"do foo"))
+;  )
+;  (println (str (redn/assoc-in nodes [:deps 'my-other-dep] {:mvn/version "0.1.2"})))
