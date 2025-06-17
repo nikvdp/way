264362092bf0725883d58bd3821b50c20bcb83e1
Robbie <robbie.huffman@nundrum.net>
2025-06-16 22:11:28 -0400

    add finding and loading central config file

--- src/way/conf.clj
+++ src/way/conf.clj
@@ -1,5 +1,4 @@
-(ns way.conf
-  (:require [clojure.java.io :as io]
+(ns way.conf (:require [clojure.java.io :as io]
             [babashka.fs :as fs]
             [clojure.edn :as edn]))
 
@@ -8,29 +7,41 @@
   []
   (let [cwd (fs/cwd)]
     (loop [wd cwd]
-      (println wd)
       (cond 
         (.exists (fs/file wd ".wayf")) (fs/file wd ".wayf")
         (nil? (fs/parent wd))          nil 
         :else (recur (fs/parent wd))
-        ))
-    ))
+        ))))
 
 (def template-wayf 
   {:search-keys []
    :direntry-note "Note displayed upon entry to this directory"
    :usage-reminders {:example ["Structured usage reminders"]}
-   :commands {:lsa "ls -a"}
+   :commands {:example "echo 'this is an example command'"}
    })
+
 
 (defn create-wayf []
   (spit ".wayf" template-wayf)
-  (fs/file ".wayf")
-  )
-
-(defn expert
-  "Set or toggle 'expert mode' setting"
-  [args]
+  (fs/file ".wayf"))
 
 
-  )
+(defn find-conf 
+  "Find central config file based on OS"
+  []
+  (let [osname   (System/getProperty "os.name")
+        conffile (cond 
+                   (re-matches #"(?i).*win.*" osname) "C:\\ProgramData\\wayf\\wayf.edn"
+                   (re-matches #"(?i).*mac" osname) "/System/Library/wayf/wayf.edn"
+                   :else "/etc/wayf/wayf.edn")
+        ]
+    conffile))
+
+(defn load-conf []
+  (try
+    (let [file (find-conf)
+          raw  (slurp file)
+          data (edn/read-string raw)]
+    data)
+    (catch java.io.FileNotFoundException nil)
+  ))
