b0dc23601aa74004234dd89a4e862fdfb06c8aea
Robbie <robbie.huffman@nundrum.net>
2025-06-19 14:14:10 -0400

    better error output

--- src/way/main.clj
+++ src/way/main.clj
@@ -2,22 +2,36 @@
  (:require [way.notecheck :as notecheck]
            [way.runner :as runner]
            [way.shell :as shell]
-;           [way.edit :as edit]
+           [way.edit :as edit]
+           [way.server :as server]
+           [way.wprint :as wprint]
 ;           [way.search :as search]
           ; [babashka.cli :as cli]
-           )
-)
-(defn -main [& args]
-(case (first args)
-  "notecheck" (notecheck/check (rest args))
-  "show"      (notecheck/show (rest args))
-  "run"       (runner/run (rest args))
-  "shell"     (shell/shell (rest args))
-;  "search"    (search/search (rest args))
-;  "edit"      (edit/edit (rest args))
-  ;"keep" - keep shell history?
-  (println "no match")
-  )
-  )
+           ))
 
+
+(defn -main [& args]
+  (case (first args)
+    "notecheck"     (notecheck/check (rest args))
+    "show"          (notecheck/show (rest args))
+    "login"         (notecheck/login (rest args))
+    "run"           (runner/run (rest args))
+    "shell"         (shell/shell (rest args))
+    ;"new"           (edit/new (rest args))
+    "edit"          (edit/edit (rest args))
+    "sync"          (server/sync (rest args))
+    "serve"         (server/cli-serve (rest args))
+    "systemd-serve" (server/systemd-serve (rest args))
+
+    ;"expert"        (conf/expert (rest args))
+    ;"keep" - keep shell history?
+    ;  "search"    (search/search (rest args))
+    ;  "edit"      (edit/edit (rest args))
+    (do
+      (println "way: no subcommand match")
+      (println "please try one of: notecheck, show, login, run, shell, edit, sync, serve, systemd-serve")
+      (System/exit 1)
+      )
+
+  ))
 
